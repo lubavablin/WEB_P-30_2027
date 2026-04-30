71dca59d15e085d78148a7f86386d1cd23376efe
Moroz07 <morozov-f.morozov@yandex.ru>
2026-04-30 08:19:36 +0300

    перевели все страницы на  bootstrap. Реализовали темную тему и её переключение. Реализовали обязательную проверку полей с выводом ошибки и добавили карточки

---
+++ Model/Author.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStore.Model
+{
+    public class Author : EFModel
+    {
+        public List<Book> Books { get; set; } = new List<Book>();
+    }
+   
+}
--- Model/Book.cs
+++ Model/Book.cs
@@ -1,10 +1,16 @@
-﻿namespace BookStore.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Model
 {
     public class Book : EFModel
     {
+        [Required(ErrorMessage = "Ввдите название")]
         public string Title { get; set; } = string.Empty;
-        public string Author { get; set; } = string.Empty;
+        public Author Author { get; set; } = new();
+        public int AuthorID { get; set; }
+        [Range(1, 1000000, ErrorMessage = "Цена должна быть от 1 до 1000000")]
         public int Price { get; set; }
+        [Range(1, 1000, ErrorMessage = "Колличество должно быть от 1 до 10000")]
         public int Quantity { get; set; }
     }
 }
