be982d6489c06306dfe270baebe6f7ff613661db
lubavablin <lubovzencenko098@gmail.com>
2026-07-02 13:09:33 +0300

    исправляем

--- BookStoreTest/obj/Debug/net9.0/BookStoreTest.AssemblyInfo.cs
+++ BookStoreTest/obj/Debug/net9.0/BookStoreTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStoreTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da50eba4d363e3cfd748432cfeaa3b1a6d6a674")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6e77eb0338f39d7c23e0f238c7a53b0ff4e10ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStoreTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStoreTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
